0a123acff3f2a566d4165ec011218cd2d3824cfa
Soham Roy <sohamroy@sohamroy.me>
2020-12-11 22:51:57 -0600

    Improved tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,18 +29,25 @@
 
 use std::{
 	error::Error,
-	fmt::{self, Debug, Display, Formatter},
+	fmt::{self, Binary, Debug, Display, Formatter},
 	marker::PhantomData,
 };
 
-use num_traits::{FromPrimitive, PrimInt, Unsigned};
+use num_traits::{FromPrimitive, One, PrimInt, Unsigned, Zero};
 
 #[cfg(test)]
 mod tests {
-	use std::{fmt::Debug, hash::Hash};
+	use std::{
+		fmt::{Binary, Debug},
+		hash::Hash,
+	};
 
 	use num_traits::FromPrimitive;
-	use rand::distributions::{Distribution, Uniform};
+	use rand::{
+		distributions::{Distribution, Uniform},
+		prelude::StdRng,
+		SeedableRng,
+	};
 
 	use crate::{tests::model::ModelWindow, PrimUInt, SlidingWindow};
 
@@ -111,10 +118,11 @@ mod tests {
 
 		if let Ok(check) = expected {
 			if check.is_some() {
-				if print {
+				let result = actual.is_ok();
+				if print || !result {
 					eprint!("Accept");
 				}
-				actual.is_ok()
+				result
 			} else {
 				// handle uncertain
 				if print {
@@ -123,30 +131,33 @@ mod tests {
 				true
 			}
 		} else {
-			if print {
+			let result = actual.is_err();
+			if print || !result {
 				eprint!("Reject");
 			}
-			actual.is_err()
+			result
 		}
 	}
 
-	fn random_increment<B: PrimUInt, N: PrimUInt + Hash + Debug>(
+	fn random_increment<B: PrimUInt + Debug + Binary, N: PrimUInt + Hash + Debug>(
 		minimum_window_size: usize,
 		max_sub: usize,
 		max_add: usize,
 		iters: usize,
 		print: bool,
+		seed: Option<u64>,
 	)
 	{
 		if print {
 			eprintln!(
-				"Block Type: {:?}, Nonce Type: {:?}, Minimum Window Size: {:?}, Max Sub: {:?}, Max Add: {:?}, Iters: {:?}",
+				"Block Type: {:?}, Nonce Type: {:?}, Minimum Window Size: {:?}, Max Sub: {:?}, Max Add: {:?}, Iters: {:?}, Seed: {:?}",
 				std::any::type_name::<B>(),
 				std::any::type_name::<N>(),
 				minimum_window_size,
 				max_sub,
 				max_add,
-				iters
+				iters,
+				seed,
 			);
 		}
 
@@ -156,20 +167,26 @@ mod tests {
 		let mut nonce = N::zero();
 
 		let rand_range = &mut Uniform::from(0..max_sub + max_add);
-		let rng = &mut rand::thread_rng();
+		let rng = &mut seed
+			.map(StdRng::seed_from_u64)
+			.unwrap_or_else(StdRng::from_entropy);
 
 		for _ in 0..iters {
 			let diff = (rand_range.sample(rng) as i64) - (max_sub as i64);
 			nonce = if diff.is_negative() {
-				nonce.saturating_sub(FromPrimitive::from_i64(diff.abs()).unwrap())
+				nonce.saturating_sub(
+					FromPrimitive::from_i64(diff.abs()).unwrap_or_else(N::max_value),
+				)
 			} else {
-				nonce.saturating_add(FromPrimitive::from_i64(diff).unwrap())
+				nonce.saturating_add(FromPrimitive::from_i64(diff).unwrap_or_else(N::max_value))
 			};
 
-			if print {
-				eprintln!("Nonce: {:?}", nonce);
+			let result = check(model_window, window, nonce, print);
+			if print || !result {
+				eprintln!(" {:?}", nonce);
+				eprintln!("{:?}", window);
 			}
-			assert!(check(model_window, window, nonce, print));
+			assert!(result);
 		}
 
 		if print {
@@ -177,8 +194,136 @@ mod tests {
 		}
 	}
 
+	fn random<B: PrimUInt + Debug + Binary, N: PrimUInt + Hash + Debug>() {
+		let seed = std::env::var("RNG_SEED")
+			.map(|seed| Some(seed.parse().unwrap()))
+			.unwrap_or_else(|_| None);
+		let print = std::env::var("PRINT_OUT")
+			.unwrap_or_else(|_| String::from("false"))
+			.parse()
+			.unwrap();
+		let iters = 1_000_000;
+
+		let window_sizes: [usize; 5] = [1, 8, 256, 2048, 8192];
+		let max_subs: [usize; 5] = [4, 64, 256, 4096, 100_000];
+		let max_adds: [usize; 5] = [4, 64, 256, 4096, 100_000];
+
+		for &window_size in window_sizes.iter() {
+			for &max_sub in max_subs.iter() {
+				for &max_add in max_adds.iter() {
+					random_increment::<B, N>(window_size, max_sub, max_add, iters, print, seed);
+				}
+			}
+		}
+	}
+
 	#[test]
-	fn random_u64_u64() { random_increment::<u64, u64>(16 * 64, 16, 64, 10_000, true); }
+	fn random_u8_u8() { random::<u8, u8>(); }
+
+	#[test]
+	fn random_u16_u8() { random::<u16, u8>(); }
+
+	#[test]
+	fn random_u32_u8() { random::<u32, u8>(); }
+
+	#[test]
+	fn random_u64_u8() { random::<u64, u8>(); }
+
+	#[test]
+	fn random_u128_u8() { random::<u128, u8>(); }
+
+	#[test]
+	fn random_usize_u8() { random::<usize, u8>(); }
+
+	#[test]
+	fn random_u8_u16() { random::<u8, u16>(); }
+
+	#[test]
+	fn random_u16_u16() { random::<u16, u16>(); }
+
+	#[test]
+	fn random_u32_u16() { random::<u32, u16>(); }
+
+	#[test]
+	fn random_u64_u16() { random::<u64, u16>(); }
+
+	#[test]
+	fn random_u128_u16() { random::<u128, u16>(); }
+
+	#[test]
+	fn random_usize_u16() { random::<usize, u16>(); }
+
+	#[test]
+	fn random_u8_u32() { random::<u8, u32>(); }
+
+	#[test]
+	fn random_u16_u32() { random::<u16, u32>(); }
+
+	#[test]
+	fn random_u32_u32() { random::<u32, u32>(); }
+
+	#[test]
+	fn random_u64_u32() { random::<u64, u32>(); }
+
+	#[test]
+	fn random_u128_u32() { random::<u128, u32>(); }
+
+	#[test]
+	fn random_usize_u32() { random::<usize, u32>(); }
+
+	#[test]
+	fn random_u8_u64() { random::<u8, u64>(); }
+
+	#[test]
+	fn random_u16_u64() { random::<u16, u64>(); }
+
+	#[test]
+	fn random_u32_u64() { random::<u32, u64>(); }
+
+	#[test]
+	fn random_u64_u64() { random::<u64, u64>(); }
+
+	#[test]
+	fn random_u128_u64() { random::<u128, u64>(); }
+
+	#[test]
+	fn random_usize_u64() { random::<usize, u64>(); }
+
+	#[test]
+	fn random_u8_u128() { random::<u8, u128>(); }
+
+	#[test]
+	fn random_u16_u128() { random::<u16, u128>(); }
+
+	#[test]
+	fn random_u32_u128() { random::<u32, u128>(); }
+
+	#[test]
+	fn random_u64_u128() { random::<u64, u128>(); }
+
+	#[test]
+	fn random_u128_u128() { random::<u128, u128>(); }
+
+	#[test]
+	fn random_usize_u128() { random::<usize, u128>(); }
+
+	#[test]
+	fn random_u8_usize() { random::<u8, usize>(); }
+
+	#[test]
+	fn random_u16_usize() { random::<u16, usize>(); }
+
+	#[test]
+	fn random_u32_usize() { random::<u32, usize>(); }
+
+	#[test]
+	fn random_u64_usize() { random::<u64, usize>(); }
+
+	#[test]
+	fn random_u128_usize() { random::<u128, usize>(); }
+
+	#[test]
+	fn random_usize_usize() { random::<usize, usize>(); }
 }
 
 /// Error returned when calling update(2) on a sliding window and the nonce has already been used
